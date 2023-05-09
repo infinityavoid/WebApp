47241e1d333902b592d403dea93d7045e8b4c6ea
infinityavoid <anton.ustinov02@mail.ru>
2023-05-09 06:30:59 +0500

    initial

---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="hello">
+      aesfas
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data(){
+          return {
+            Products:[
+                {id:1,name:'q'},
+                {id:2,name:'w'},
+                {id:3,name:'e'},
+                {id:4,name:'r'},
+                {id:5,name:'t'},
+                {id:6,name:'y'},
+                {id:7,name:'u'},
+                {id:8,name:'i'},
+            ]
+          } 
+      },
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  

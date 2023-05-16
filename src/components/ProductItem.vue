00951def6e48eb1c9ee01c4ea2897413f7fd0f73
infinityavoid <anton.ustinov02@mail.ru>
2023-05-16 23:19:09 +0500

    qwe

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -3,9 +3,9 @@
         <label>{{Product_data.name}}</label>
         <label>{{Product_data.price}}р</label>
         <div v-if="allProducts.orderItems.findIndex(item => item.id === Product_data.id) != -1">
-            <button @click="del()">-</button>
+            <button @click="del()" class="minusButton">-</button>
             <label >{{ allProducts.orderItems[allProducts.orderItems.findIndex(item => item.id === Product_data.id)].quantity }}</label>
-            <button @click="add()">+</button>
+            <button @click="add()" class="plusButton">+</button>
         </div>
         <button v-else @click="add()" class="addButton">Добавить</button>
     </div>
@@ -49,13 +49,55 @@ export default {
 </script>
 
 <style scoped>
+.minusButton
+{
+    pointer-events: auto;
+    transform: scale3d(1, 1, 1);
+    background-color: #e64d44;
+    transition: transform var(--animation), visibility var(--animation);
+    width: 38px;
+    height: 30px;
+    color: #fff;
+    outline: none;
+    border: none;
+    text-transform: uppercase;
+    display: inline-block;
+    font-family: var(--default-font);
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 18px;
+    padding: 6px 16px;
+    height: 30px;
+    border-radius: 7px;
+    box-sizing: border-box;
+    cursor: pointer;
+}
+.plusButton
+{
+    width: 38px;
+    display: inline-block;
+    font-family: var(--default-font);
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 18px;
+    padding: 6px 16px;
+    height: 30px;
+    border-radius: 7px;
+    box-sizing: border-box;
+    background-color: #f8a917;;
+    text-transform: uppercase;
+    color: #fff;
+    outline: none;
+    border: none;
+    cursor: pointer;
+}
 .item
 {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    width: 100px;
-    height: 100px;
+    width: 150px;
+    height: 150px;
     margin-bottom: 10px;
     box-shadow: 0 0 8px 0 #e0e0e0;
     border: 1px solid var(--tg-theme-button-color);

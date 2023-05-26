f998ee788f29f82c7c7bc91822e7998f349ab98d
infinityavoid <anton.ustinov02@mail.ru>
2023-05-27 03:23:30 +0500

    qwe

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,7 @@ const newFunc = async () =>
                 method:"POST",url:"http://localhost:8000/createInvoice",data:store.state.orderItems}).then(res => {
                 WebApp.openInvoice(res.data.result, (object) => 
                 {
+                    console.log(object)
                     if (object.status == 'paid')
                     {
                         store.state.tg.close()
